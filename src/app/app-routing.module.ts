10ae485741ad666403150b201ba2bd41adf9e605
Aaron Fuentes <aaron@raccoondev.studio>
2020-05-30 17:44:09 -0500

    Routes were defined

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UsersComponent } from './pages/users/users.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'usuarios',
+    component: UsersComponent
+  },
+  {
+    path:'**',
+    pathMatch: 'full',
+    redirectTo: 'usuarios'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
