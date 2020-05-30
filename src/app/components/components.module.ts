b0acbe6a62e42a69487bc500787c7652c2cbec74
Aaron Fuentes <aaron@raccoondev.studio>
2020-05-30 18:50:41 -0500

    users table header was added

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -5,11 +5,12 @@ import { SidebarComponent } from './sidebar/sidebar.component';
 import { UsersHeaderComponent } from './users-header/users-header.component';
 import { UsersTableComponent } from './users-table/users-table.component';
 import { AppRoutingModule } from '../app-routing.module';
+import { UsersTableHeaderComponent } from './users-table-header/users-table-header.component';
 
 
 
 @NgModule({
-  declarations: [HeaderComponent, SidebarComponent, UsersHeaderComponent, UsersTableComponent],
+  declarations: [HeaderComponent, SidebarComponent, UsersHeaderComponent, UsersTableComponent, UsersTableHeaderComponent],
   imports: [
     CommonModule,
     AppRoutingModule
---
+++ src/app/components/users-table-header/users-table-header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-users-table-header',
+  templateUrl: './users-table-header.component.html',
+  styleUrls: ['./users-table-header.component.scss']
+})
+export class UsersTableHeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
